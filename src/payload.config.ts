aa7b9fb046ca00c20ae3ede11f3f2893076f1eb9
Grzegorz 'Hankier' Ćwikliński <hankier@hqr.sh>
2025-01-15 11:54:07 +0100

    Change media storage

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -35,6 +35,11 @@ export default buildConfig({
     importMap: {
       baseDir: path.resolve(dirname),
     },
+    upload: {
+      limits: {
+        fileSize: 5000000, // 5MB, written in bytes
+      },
+    },
     user: Users.slug,
     livePreview: {
       breakpoints: [
